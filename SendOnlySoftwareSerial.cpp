69823a7c1044fe46597aa67aa7feb844bc4b53a1
Kemal Hadimli <kemal@hadimli.com>
2013-05-26 16:56:50 +0300

    Save 192 bytes on SendOnlySoftwareSerial by only including delay table for the speed we use

--- SendOnlySoftwareSerial.cpp
+++ SendOnlySoftwareSerial.cpp
@@ -42,6 +42,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <avr/pgmspace.h>
 #include "Arduino.h"
 #include "SendOnlySoftwareSerial.h"
+#include "config.h"
+#include "def.h"
 //
 // Lookup table
 //
@@ -59,18 +61,33 @@ typedef struct _DELAY_TABLE
 static const DELAY_TABLE PROGMEM table[] = 
 {
   //  baud    rxcenter   rxintra    rxstop    tx
+#if TELEMETRY_FRSKY_SERIAL==115200
   { 115200,   1,         17,        17,       12,    },
+#elif TELEMETRY_FRSKY_SERIAL==57600
   { 57600,    10,        37,        37,       33,    },
+#elif TELEMETRY_FRSKY_SERIAL==38400
   { 38400,    25,        57,        57,       54,    },
+#elif TELEMETRY_FRSKY_SERIAL==31250
   { 31250,    31,        70,        70,       68,    },
+#elif TELEMETRY_FRSKY_SERIAL==28800
   { 28800,    34,        77,        77,       74,    },
+#elif TELEMETRY_FRSKY_SERIAL==19200
   { 19200,    54,        117,       117,      114,   },
+#elif TELEMETRY_FRSKY_SERIAL==14400
   { 14400,    74,        156,       156,      153,   },
+#elif TELEMETRY_FRSKY_SERIAL==9600
   { 9600,     114,       236,       236,      233,   },
+#elif TELEMETRY_FRSKY_SERIAL==4800
   { 4800,     233,       474,       474,      471,   },
+#elif TELEMETRY_FRSKY_SERIAL==2400
   { 2400,     471,       950,       950,      947,   },
+#elif TELEMETRY_FRSKY_SERIAL==1200
   { 1200,     947,       1902,      1902,     1899,  },
+#elif TELEMETRY_FRSKY_SERIAL==300
   { 300,      3804,      7617,      7617,     7614,  },
+#else
+  #error "Invalid baud rate for TELEMETRY_FRSKY_SERIAL"
+#endif
 };
 
 const int XMIT_START_ADJUSTMENT = 5;
@@ -80,18 +97,33 @@ const int XMIT_START_ADJUSTMENT = 5;
 static const DELAY_TABLE table[] PROGMEM = 
 {
   //  baud    rxcenter    rxintra    rxstop  tx
+#if TELEMETRY_FRSKY_SERIAL==115200
   { 115200,   1,          5,         5,      3,      },
+#elif TELEMETRY_FRSKY_SERIAL==57600
   { 57600,    1,          15,        15,     13,     },
+#elif TELEMETRY_FRSKY_SERIAL==38400
   { 38400,    2,          25,        26,     23,     },
+#elif TELEMETRY_FRSKY_SERIAL==31250
   { 31250,    7,          32,        33,     29,     },
+#elif TELEMETRY_FRSKY_SERIAL==28800
   { 28800,    11,         35,        35,     32,     },
+#elif TELEMETRY_FRSKY_SERIAL==19200
   { 19200,    20,         55,        55,     52,     },
+#elif TELEMETRY_FRSKY_SERIAL==14400
   { 14400,    30,         75,        75,     72,     },
+#elif TELEMETRY_FRSKY_SERIAL==9600
   { 9600,     50,         114,       114,    112,    },
+#elif TELEMETRY_FRSKY_SERIAL==4800
   { 4800,     110,        233,       233,    230,    },
+#elif TELEMETRY_FRSKY_SERIAL==2400
   { 2400,     229,        472,       472,    469,    },
+#elif TELEMETRY_FRSKY_SERIAL==1200
   { 1200,     467,        948,       948,    945,    },
+#elif TELEMETRY_FRSKY_SERIAL==300
   { 300,      1895,       3805,      3805,   3802,   },
+#else
+  #error "Invalid baud rate for TELEMETRY_FRSKY_SERIAL"
+#endif
 };
 
 const int XMIT_START_ADJUSTMENT = 4;
@@ -104,18 +136,33 @@ const int XMIT_START_ADJUSTMENT = 4;
 static const DELAY_TABLE PROGMEM table[] =
 {
   //  baud    rxcenter    rxintra    rxstop  tx
+#if TELEMETRY_FRSKY_SERIAL==115200
   { 115200,   3,          21,        21,     18,     },
+#elif TELEMETRY_FRSKY_SERIAL==57600
   { 57600,    20,         43,        43,     41,     },
+#elif TELEMETRY_FRSKY_SERIAL==38400
   { 38400,    37,         73,        73,     70,     },
+#elif TELEMETRY_FRSKY_SERIAL==31250
   { 31250,    45,         89,        89,     88,     },
+#elif TELEMETRY_FRSKY_SERIAL==28800
   { 28800,    46,         98,        98,     95,     },
+#elif TELEMETRY_FRSKY_SERIAL==19200
   { 19200,    71,         148,       148,    145,    },
+#elif TELEMETRY_FRSKY_SERIAL==14400
   { 14400,    96,         197,       197,    194,    },
+#elif TELEMETRY_FRSKY_SERIAL==9600
   { 9600,     146,        297,       297,    294,    },
+#elif TELEMETRY_FRSKY_SERIAL==4800
   { 4800,     296,        595,       595,    592,    },
+#elif TELEMETRY_FRSKY_SERIAL==2400
   { 2400,     592,        1189,      1189,   1186,   },
+#elif TELEMETRY_FRSKY_SERIAL==1200
   { 1200,     1187,       2379,      2379,   2376,   },
+#elif TELEMETRY_FRSKY_SERIAL==300
   { 300,      4759,       9523,      9523,   9520,   },
+#else
+  #error "Invalid baud rate for TELEMETRY_FRSKY_SERIAL"
+#endif
 };
 
 const int XMIT_START_ADJUSTMENT = 6;
